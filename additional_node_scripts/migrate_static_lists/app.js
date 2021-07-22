0d8cc1afdd51163c138447680c07ba27fe40f4d6
Philip Porwisch <philipporwisch@web.de>
2021-07-22 14:39:53 +0200

    Step one of Static List Migration

--- additional_node_scripts/migrate_static_lists/app.js
+++ additional_node_scripts/migrate_static_lists/app.js
@@ -1,14 +1,45 @@
-function Person(name, age, eyeColor) {
-    this.name = name;
-    this.age = age;
-    this.eyeColor = eyeColor;
-    this.updateAge = function() {
-        this.age = ++this.age;
-    };
+require('dotenv').config({ path: '../../.env'});
+const apiKeyOrigin = process.env.HAPIKEY_ORIGIN;
+const apiKeyTarget = process.env.HAPIKEY_TARGET;
+const StaticList = require('./staticListConstructor');
+const axios = require('axios');
+const fs = require('fs');
+
+const getListsEndpoint = 'https://api.hubapi.com/contacts/v1/lists/static?hapikey=' + apiKeyOrigin;
+const createListEndpoint = 'https://api.hubapi.com/contacts/v1/lists?hapikey=' + apiKeyTarget;
+
+const getStaticListsOrigin = async () => {     
+    try {
+        const res = await axios.get(getListsEndpoint);
+        const data = res.data;
+        return data;
+        
+    } catch(err) {
+         console.error(err);
+    }
 }
 
-let person01 = new Person('Philip', 26, 'Brown');
+const cloneListsIntoTarget = async (data) => {
+    let finalJson = '{\n'
 
-console.log(person01);
-person01.updateAge;
-console.log(person01.age);
+    for(let i=0; i<data.lists.length; i++) {
+        let listName = data.lists[i].name;
+        let listOldId= data.lists[i].listId;
+        try {
+            const res = await axios.post(createListEndpoint,{"name":`${listName}`});
+            listNewId = res.data.listId;
+            finalJson = finalJson + `"${listOldId}":"${listNewId}", \n`
+            console.log(i+ ': List created successfully');
+        } catch(err) {
+            finalJson = finalJson + `"${listOldId}":ERROR, `;
+            console.log(i + ': ' +err);
+        }
+    }
+    finalJson = finalJson.slice(0, -3) + '\n}';
+    fs.writeFileSync('../../results/listIdMapping.json', finalJson);
+}
+
+
+
+getStaticListsOrigin()
+    .then(data => cloneListsIntoTarget(data));
