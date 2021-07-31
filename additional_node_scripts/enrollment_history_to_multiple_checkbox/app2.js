53a483c90c841f2b8f4663a3e92bec8fb133b998
S. Reyes <sreyes@hubspot.com>
2021-07-31 15:51:00 -0400

    test function to roll up WF Ids by unique Vids

--- additional_node_scripts/enrollment_history_to_multiple_checkbox/app2.js
+++ additional_node_scripts/enrollment_history_to_multiple_checkbox/app2.js
@@ -2,6 +2,31 @@ require('dotenv').config();
 const fs = require('fs');
 const axios = require('axios').default;
 const targetHapiKey = process.env.HAPIKEY_TARGET;
-const contactsByList = require('./results/contactsByList.json');
+const wfsData = require('./results/contactsByList');
 
-console.log(contactsByList);
+// Should pull Email instead to POST to Target Portal 
+
+const concatWorkflowsByVid = () => {
+    const array = wfsData.contacts_by_list;
+
+    const list = array.reduce((newObj, wfInstance) => {
+
+        let wfId = Object.keys(wfInstance)[0]; 
+
+        if (wfInstance[wfId].length) {
+            wfInstance[wfId].forEach(contact => {
+                let vid = contact.vid;
+
+                newObj.hasOwnProperty(vid) ? newObj[vid] = newObj[vid] += `, ${wfId}` : newObj[vid] = wfId;
+                
+            });
+        }
+        
+        return newObj;
+        
+    }, {});
+
+    console.log(list);
+}
+
+concatWorkflowsByVid();
