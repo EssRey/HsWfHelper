cdec428d95d3c6308181b9ec623c9b8e1747cd7a
S. Reyes <sreyes@hubspot.com>
2021-08-01 13:10:20 -0400

    crete multicheck property in target portal with all WF Ids

--- additional_node_scripts/enrollment_history_to_multiple_checkbox/app2.js
+++ additional_node_scripts/enrollment_history_to_multiple_checkbox/app2.js
@@ -16,8 +16,10 @@ const concatWorkflowsByVid = () => {
         if (wfInstance[wfId].length) {
             wfInstance[wfId].forEach(contact => {
                 let vid = contact.vid;
+                // let email = contact['identity-profiles'][0].identities[0].value;
 
                 newObj.hasOwnProperty(vid) ? newObj[vid] = newObj[vid] += `, ${wfId}` : newObj[vid] = wfId;
+                // newObj.hasOwnProperty(email) ? newObj[email] = newObj[email] += `, ${wfId}` : newObj[email] = wfId;
                 
             });
         }
@@ -29,4 +31,36 @@ const concatWorkflowsByVid = () => {
     console.log(list);
 }
 
-concatWorkflowsByVid();
+const createMulticheckProperty = () => {
+    const propertyOptions = [];
+
+    // TODO: Refactor - Already looping through contacts_by_list in concatWorkflowsByVid for wfId
+
+    wfsData.contacts_by_list.forEach(wfInstance => {
+        let wfId = Object.keys(wfInstance)[0];
+        propertyOptions.push({"label": wfId, "value": wfId});
+    })
+
+    const propertyObj = {
+        "name": "workflow_ids",
+        "label": "Workflow IDs",
+        "description": "All workflow IDs (flowId_id) from origin portal",
+        "groupName": "contactinformation",
+        "type": "enumeration",
+        "fieldType": "checkbox",
+        "hidden": false,
+        "options": propertyOptions,
+        "formField": false
+    };
+
+    axios.post(`https://api.hubapi.com/properties/v1/contacts/properties?hapikey=${targetHapiKey}`, propertyObj)
+        .then(res => {
+            console.log(res);
+        })
+        .catch(e => {
+            console.log(e);
+        });
+}
+
+// concatWorkflowsByVid();
+createMulticheckProperty();
