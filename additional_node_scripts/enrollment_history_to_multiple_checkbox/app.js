2a5dc5d73a1247d21baf59429ae78faebc63340d
S. Reyes <sreyes@hubspot.com>
2021-08-31 18:11:45 -0400

    Testing pagination and rate limits

--- additional_node_scripts/enrollment_history_to_multiple_checkbox/app.js
+++ additional_node_scripts/enrollment_history_to_multiple_checkbox/app.js
@@ -5,6 +5,14 @@ const originHapiKey = process.env.HAPIKEY_ORIGIN;
 
 // Todo: Error handling
 
+const timer = () => new Promise(res => setTimeout(res, 10000));
+
+const delayMessage = async (listId) => { 
+    console.log('API Burst Limit reached... Pausing for 10 seconds')
+    await timer();
+    console.log(`Resuming on list #${listId}`);
+}
+
 const getContactIdLists = async () => {
 
     const response = await axios.get(`https://api.hubapi.com/automation/v3/workflows/?hapikey=${originHapiKey}`);
@@ -16,7 +24,7 @@ const getContactIdLists = async () => {
         throw new Error('Could not get workflows');
     }
 
-    let contactListIds = [];
+    const contactListIds = [];
 
     for (workflow of workflowsData) {
         contactListIds.push({
@@ -33,33 +41,75 @@ const getContactsByListId = async () => {
 
     const contactIdLists = await getContactIdLists();
 
+    let limit = 100;
+
+    let lastContacts, runningContacts;
+
+    let workflowContacts = [];
+
     // loop through idLists and call list endpoints
 
-    const result = await Promise.all(contactIdLists.map(workflow => {
-        const listURL = `https://api.hubapi.com/contacts/v1/lists/${workflow.contactListIds.enrolled}/contacts/all?hapikey=${originHapiKey}`
+    for (let i = 0; i < contactIdLists.length; i++) {
 
-        return axios.get(listURL)
-            .then(res => {
+        const workflow = contactIdLists[i];
 
-                const key = workflow.flowId_workflowId;
+        let listURL = `https://api.hubapi.com/contacts/v1/lists/${workflow.contactListIds.enrolled}/contacts/all?hapikey=${originHapiKey}&count=100&vidOffset=`;
 
-                return {
-                    [key]: res.data.contacts
-                };
+        console.log(`Getting contacts from list #${workflow.contactListIds.enrolled} (${(i + 1)} of ${contactIdLists.length})`);
+
+        let response = await axios.get(listURL);
+
+        lastContacts = response.data.contacts;
+
+        runningContacts = lastContacts;
+
+        let offset = response.data['vid-offset'];
+
+        limit = response.headers['x-hubspot-ratelimit-remaining'];
+
+        // Paginate if 'has-more' = true with 'vid-offset'
+
+        while (response.data['has-more']) {
+
+            listURL = `https://api.hubapi.com/contacts/v1/lists/${workflow.contactListIds.enrolled}/contacts/all?hapikey=${originHapiKey}&count=100&vidOffset=${offset}`;
+
+            response = await axios.get(listURL);
+
+            lastContacts = response.data.contacts;
+
+            runningContacts = [...runningContacts, ...lastContacts];
+
+            offset = response.data['vid-offset'];
+
+            limit = response.headers['x-hubspot-ratelimit-remaining'];
+
+            // Pause execution for 10 seconds if rate limit remaining nears buffer (10 for testing)
+
+            if (limit < 10) {
+                await delayMessage(workflow.contactListIds.enrolled);
             }
-        );
-    }));
 
-    writeContactsByListFile(result);
+        }
 
-    return result;
+        const key = workflow.flowId_workflowId;
 
+        const workflowObj = {
+            [key]: runningContacts
+        }
+
+        workflowContacts.push(workflowObj);
+
+    }
+
+    writeContactsByListFile(workflowContacts);
+
+    return workflowContacts;
 }
 
 getContactsByListId()
 
-function writeContactsByListFile(contactsByList) {
-    let contactsByListJson = {"contacts_by_list": contactsByList};
+const writeContactsByListFile = contactsByList => {
+    const contactsByListJson = {"contacts_by_list": contactsByList};
     testFile = JSON.stringify(contactsByListJson, null, 2);
     fs.writeFileSync('results/contactsByList.json', testFile);
 }
