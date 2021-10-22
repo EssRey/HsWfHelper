71ef6cf5932297cfcd075b9d0567295ad22259e6
Philip Porwisch <79708851+Flippsn@users.noreply.github.com>
2021-10-22 12:16:20 +0200

    Add files via upload

--- additional_node_scripts/dynamic_list_membership_to_multiple_checkbox/readActiveLists.js
+++ additional_node_scripts/dynamic_list_membership_to_multiple_checkbox/readActiveLists.js
@@ -1,10 +1,15 @@
 require('dotenv').config({ path: '../../.env'});
+const prompt = require('prompt-sync')({sigint: true});
 const apiKeyOrigin = process.env.HAPIKEY_ORIGIN;
 const apiKeyTarget = process.env.HAPIKEY_TARGET;
 const axios = require('axios');
 const fs = require('fs');
 const { url } = require('inspector');
 let callCounter = 0;
+let error = false;
+
+console.log('\nThis script takes a numerical value for "listID". Only lists with an ID higher than the provided value will be included in the execution.\n\nIf you are starting from scratch type: 0');
+let startId = prompt('Please provide a listID now: ');
 
 
 //Format required endpoints
@@ -26,6 +31,7 @@ const countCalls = () => {
     if(callCounter == 95) {
         console.log('Burst limit reached. Pausing execution for 10 seconds');
         sleep(10000);
+        console.log('Continuing script...');
         return callCounter = 0;
     }else{
         return callCounter+=1;
@@ -34,24 +40,47 @@ const countCalls = () => {
 
 //
 const getContactEmailsInList = async (uniqueListId) => {
-    countCalls();
+    console.log(`Reading list ${uniqueListId}`);
     getContactsInListEndpoint = `https://api.hubapi.com/contacts/v1/lists/${uniqueListId}/contacts/all?hapikey=${apiKeyOrigin}&property=email&count=100`;
-    let res = await axios.get(getContactsInListEndpoint);
+    do {
+        error = false;
+        try {
+            countCalls();
+            var res = await axios.get(getContactsInListEndpoint);
+
+        }
+        catch(error) {
+            error = true;
+            console.log('Error occured. Retrying in 2 seconds');
+            sleep(2000);
+        }
+    } while (error);
     let contactArray = res.data.contacts;
     let emailArray = [];
     let placeholderArray
     if (contactArray) emailArray = contactArray.map(contact => contact.properties.email ? contact.properties.email.value : "noEmail"); 
     vidOffset = res.data['vid-offset']; 
     while(res.data['has-more']) {
-        countCalls();
-        getContactsInListEndpoint = `https://api.hubapi.com/contacts/v1/lists/${uniqueListId}/contacts/all?hapikey=${apiKeyOrigin}&property=email&vidOffset=${vidOffset}`;
-        res = await axios.get(getContactsInListEndpoint);
+        getContactsInListEndpoint = `https://api.hubapi.com/contacts/v1/lists/${uniqueListId}/contacts/all?hapikey=${apiKeyOrigin}&property=email&vidOffset=${vidOffset}&count=100`;
+        do {
+            error = false;
+            try {
+                countCalls();
+                res = await axios.get(getContactsInListEndpoint);
+            }
+            catch(error) {
+                error = true;
+                console.log('Error occured. Retrying in 2 seconds');
+                sleep(2000);
+            }
+        } while (error);
         contactArray = res.data.contacts;
         if (contactArray) placeholderArray = contactArray.map( contact => contact.properties.email ? contact.properties.email.value : "noEmail");
         emailArray = [...emailArray, ...placeholderArray];
         vidOffset = res.data['vid-offset'];
     }
     emailArray.sort((a,b) => a>b?1:-1);
+    emailArray = [...new Set(emailArray)];
     return emailArray;
 }
 
@@ -66,8 +95,10 @@ const getActiveListsOrigin = async () => {
         const res = await axios.get(getListsEndpoint);
         let listIdArray = [];
         for(let i=0; i< res.data.lists.length; i++) {
-            listIdArray.push(res.data.lists[i].listId);
-        }
+            if(res.data.lists[i].listId > startId) {
+                listIdArray.push(res.data.lists[i].listId);
+                }
+            }
         return listIdArray;
         
     } catch(err) {
@@ -79,6 +110,7 @@ const getActiveListsOrigin = async () => {
 const createListFiles = async (listIdArray) => {
     let uniqueListId = 0;
     for(let i=0; i<listIdArray.length; i++) {
+        
         uniqueListId = listIdArray[i];
         const emailArray = await getContactEmailsInList(uniqueListId);
         fs.writeFileSync(`listsWithContacts/list_${uniqueListId}.txt`, emailArray.toString());
