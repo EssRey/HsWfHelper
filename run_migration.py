67dc4cba0418baac9d150c79a7f5bae056055b93
viktorbrech <viktor.brech@gmail.com>
2021-11-20 20:38:41 +0100

    remove input file creation from auto migration

--- run_migration.py
+++ run_migration.py
@@ -3,8 +3,9 @@ from list_copy import copy_all_lists
 from workflow_copy import copy_all_workflows
 import logger
 # create_input_files will create "inputs/id_mappings.json" and 'inputs/active_list_ids.json'
-import create_input_files
+## comment this out for now, run create_input_files.py manually first instead
+#import create_input_files
 
 copy_all_lists(simulate=True)
-copy_all_workflows(simulate=True)
-logger.write_log("full_migration_log")
+#copy_all_workflows(simulate=True)
+logger.write_log("simulates_list_copy_v1")
